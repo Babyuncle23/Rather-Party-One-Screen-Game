91e9f4cdee5b0b748030b5efa946e4123fa70c82
Babyuncle23 <aboriginal2390@gmail.com>
2026-07-29 09:45:55 +0300

    scenes in between v1

---
+++ src/data/scenes.js
@@ -0,0 +1,808 @@
+export const SCENE_REGISTRY = {
+   "102": {
+    "bg": {
+      "url": "",
+      "local": "",
+      "x": 50,
+      "y": 50,
+      "s": 100
+    },
+    "elements": [
+      {
+        "id": 1,
+        "type": "emoji",
+        "content": "[RESPONDER]",
+        "x": 30,
+        "y": 50,
+        "s": 1,
+        "r": 0,
+        "flipX": true,
+        "anim": "anim-run-left",
+        "emo": {
+          "enabled": false
+        }
+      },
+      {
+        "id": 2,
+        "type": "emoji",
+        "content": "👀",
+        "x": 30,
+        "y": "45",
+        "s": 1.05,
+        "r": 7,
+        "flipX": false,
+        "anim": "anim-run-left",
+        "emo": {
+          "enabled": false
+        }
+      },
+      {
+        "id": 3,
+        "type": "text",
+        "content": "[GUESS_WORD]",
+        "x": 64,
+        "y": 50,
+        "s": 0.5,
+        "r": 0,
+        "flipX": false,
+        "anim": "anim-run-left",
+        "color": "#ffffff",
+        "font": "inherit",
+        "emo": {
+          "enabled": false
+        }
+      },
+      {
+        "id": 4,
+        "type": "emoji",
+        "content": "👁️",
+        "x": 48,
+        "y": 34,
+        "s": 1,
+        "r": 0,
+        "flipX": false,
+        "anim": "anim-run-left",
+        "emo": {
+          "enabled": false
+        }
+      },
+      {
+        "id": 5,
+        "type": "emoji",
+        "content": "👁️",
+        "x": 71,
+        "y": 33,
+        "s": 1,
+        "r": 0,
+        "flipX": false,
+        "anim": "anim-run-left",
+        "emo": {
+          "enabled": false
+        }
+      },
+      {
+        "id": 6,
+        "type": "emoji",
+        "content": "👄",
+        "x": 60,
+        "y": 59,
+        "s": 1,
+        "r": 0,
+        "flipX": false,
+        "anim": "anim-run-left",
+        "emo": {
+          "enabled": false
+        }
+      }
+    ]
+  }
+,
+  "104": {
+    bg: { url: "", local: "", x: 50, y: 50, s: 100 },
+    elements: [
+      { id: 1, type: "emoji", content: "🏳️", x: "53", y: "53", s: "3.95", r: 0, flipX: false, anim: "none", emo: { enabled: false } },
+      { id: 2, type: "emoji", content: "[RESPONDER]", x: 53, y: 40, s: 1.35, r: 0, flipX: false, anim: "none", emo: { enabled: true } },
+{ id: 3, type: "text", content: "[GUESS_WORD]", x: "51", y: "85", s: 0.55, r: 0, flipX: false, anim: "none", color: "#ffffff", font: "inherit", emo: { enabled: false } }    ]
+  },
+  "106": {
+    "bg": {
+      "url": "",
+      "local": "",
+      "x": 50,
+      "y": 50,
+      "s": 100
+    },
+    "elements": [
+      {
+        "id": 2,
+        "type": "emoji",
+        "content": "💸",
+        "x": 35,
+        "y": 50,
+        "s": 2.45,
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "emo": {
+          "enabled": false
+        }
+      },
+      {
+        "id": 1,
+        "type": "emoji",
+        "content": "[RESPONDER]",
+        "x": "68",
+        "y": "46",
+        "s": "2.3",
+        "r": 0,
+        "flipX": true,
+        "anim": "none",
+        "emo": {
+          "enabled": true
+        }
+      },
+      {
+        "id": 5,
+        "type": "text",
+        "content": "[GUESS_WORD]",
+        "x": "36",
+        "y": "49",
+        "s": 0.3,
+        "r": -43,
+        "flipX": false,
+        "anim": "none",
+        "color": "#09090d",
+        "font": "serif",
+        "emo": {
+          "enabled": false
+        }
+      }
+    ]
+  }
+,
+  "fanfic": {
+    "bg": {
+      "url": "",
+      "local": "",
+      "x": "51",
+      "y": "18",
+      "s": 100
+    },
+    "elements": [
+      {
+        "id": 1,
+        "type": "emoji",
+        "content": "[RESPONDER]",
+        "x": "72",
+        "y": "47",
+        "s": "3",
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "emo": {
+          "enabled": true
+        }
+      },
+      {
+        "id": 2,
+        "type": "emoji",
+        "content": "📖",
+        "x": "49",
+        "y": "88",
+        "s": "2.4",
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "emo": {
+          "enabled": false
+        }
+      },
+      {
+        "id": 3,
+        "type": "text",
+        "content": "ALL ABOUT",
+        "x": "27",
+        "y": "13",
+        "s": "0.6",
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "color": "#ffffff",
+        "font": "serif",
+        "emo": {
+          "enabled": false
+        }
+      },
+      {
+        "id": 4,
+        "type": "text",
+        "content": "[GUESS_WORD]",
+        "x": "27",
+        "y": "40",
+        "s": "1.1",
+        "r": "0",
+        "flipX": false,
+        "anim": "anim-shake",
+        "color": "#37ffe2",
+        "font": "serif",
+        "emo": {
+          "enabled": false
+        }
+      }
+    ]
+  }
+,
+  "drink": {
+    "bg": {
+      "url": "",
+      "local": "",
+      "x": 50,
+      "y": 50,
+      "s": 100
+    },
+    "elements": [
+      {
+        "id": 1,
+        "type": "emoji",
+        "content": "[RESPONDER]",
+        "x": "51",
+        "y": 46,
+        "s": "2.55",
+        "r": 2,
+        "flipX": false,
+        "anim": "none",
+        "emo": {
+          "enabled": true
+        }
+      },
+      {
+        "id": 2,
+        "type": "emoji",
+        "content": "🧪",
+        "x": "30",
+        "y": 41,
+        "s": 0.8,
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "emo": {
+          "enabled": false
+        },
+        "glow": true
+      },
+      {
+        "id": 5,
+        "type": "text",
+        "content": "?",
+        "x": "68",
+        "y": "82",
+        "s": 1,
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "color": "#94ff29",
+        "font": "inherit",
+        "emo": {
+          "enabled": false
+        }
+      },
+      {
+        "id": 7,
+        "type": "text",
+        "content": "[GUESS_WORD]",
+        "x": "45",
+        "y": "84",
+        "s": "0.8",
+        "r": "0",
+        "flipX": false,
+        "anim": "none",
+        "color": "#94ff29",
+        "font": "Impact",
+        "emo": {
+          "enabled": false
+        }
+      }
+    ]
+  }
+,
+  "switch": {
+    "bg": {
+      "url": "",
+      "local": "",
+      "x": 50,
+      "y": 50,
+      "s": 100
+    },
+    "elements": [
+      {
+        "id": 5,
+        "type": "emoji",
+        "content": "🔄",
+        "x": "50",
+        "y": "72",
+        "s": "0.75",
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "emo": {
+          "enabled": false
+        }
+      },
+      {
+        "id": 1,
+        "type": "emoji",
+        "content": "👨‍⚕️",
+        "x": 31,
+        "y": 65,
+        "s": 1,
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "emo": {
+          "enabled": false
+        }
+      },
+      {
+        "id": 2,
+        "type": "emoji",
+        "content": "[RESPONDER]",
+        "x": 31,
+        "y": "43",
+        "s": "2",
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "emo": {
+          "enabled": true
+        }
+      },
+      {
+        "id": 3,
+        "type": "emoji",
+        "content": "👨‍🔧",
+        "x": 66,
+        "y": 64,
+        "s": 1,
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "emo": {
+          "enabled": false
+        }
+      },
+      {
+        "id": 4,
+        "type": "emoji",
+        "content": "[PICKER]",
+        "x": "66",
+        "y": "44",
+        "s": "1.95",
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "emo": {
+          "enabled": true
+        }
+      }
+    ]
+  }
+,
+  "dog": {
+    "bg": {
+      "url": "",
+      "local": "",
+      "x": 50,
+      "y": 50,
+      "s": 100
+    },
+    "elements": [
+      {
+        "id": 1,
+        "type": "emoji",
+        "content": "🐕",
+        "x": 69,
+        "y": 70,
+        "s": "2.25",
+        "r": 0,
+        "flipX": false,
+        "anim": "anim-bounce-talk",
+        "emo": {
+          "enabled": false
+        }
+      },
+      {
+        "id": 2,
+        "type": "text",
+        "content": "[GUESS_WORD]",
+        "x": "59",
+        "y": "16",
+        "s": "0.75",
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "color": "#ffffff",
+        "font": "inherit",
+        "emo": {
+          "enabled": false
+        }
+      },
+      {
+        "id": 3,
+        "type": "emoji",
+        "content": "[RESPONDER]",
+        "x": "23",
+        "y": "33",
+        "s": "2",
+        "r": 16,
+        "flipX": false,
+        "anim": "none",
+        "emo": {
+          "enabled": true
+        }
+      },
+      {
+        "id": 4,
+        "type": "text",
+        "content": "is a really",
+        "x": "64",
+        "y": "36",
+        "s": "0.35",
+        "r": "-8",
+        "flipX": false,
+        "anim": "none",
+        "color": "#ffffff",
+        "font": "inherit",
+        "emo": {
+          "enabled": false
+        }
+      },
+      {
+        "id": 5,
+        "type": "text",
+        "content": "good",
+        "x": "19",
+        "y": "71",
+        "s": "0.5",
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "color": "#ffffff",
+        "font": "inherit",
+        "emo": {
+          "enabled": false
+        }
+      },
+      {
+        "id": 6,
+        "type": "text",
+        "content": "boy!",
+        "x": "42",
+        "y": "84",
+        "s": "0.65",
+        "r": "-1",
+        "flipX": false,
+        "anim": "none",
+        "color": "#ffffff",
+        "font": "inherit",
+        "emo": {
+          "enabled": false
+        }
+      }
+    ]
+  }
+,
+  "island": {
+    "bg": {
+      "url": "",
+      "local": "litemon-island-4888462.jpg",
+      "cover": true,
+      "x": "47",
+      "y": "42",
+      "s": 100
+    },
+    "elements": [
+      {
+        "id": 1,
+        "type": "emoji",
+        "content": "[RESPONDER]",
+        "x": "56",
+        "y": "41",
+        "s": 1.55,
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "emo": {
+          "enabled": true
+        }
+      },
+      {
+        "id": 2,
+        "type": "text",
+        "content": "Well, I think ",
+        "x": "27",
+        "y": "15",
+        "s": "0.95",
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "color": "#ffffff",
+        "font": "Impact",
+        "emo": {
+          "enabled": false
+        }
+      },
+      {
+        "id": 3,
+        "type": "text",
+        "content": "[GUESS_WORD]",
+        "x": "27",
+        "y": "42",
+        "s": "0.65",
+        "r": "0",
+        "flipX": false,
+        "anim": "none",
+        "color": "#ffffff",
+        "font": "Impact",
+        "emo": {
+          "enabled": false
+        }
+      },
+      {
+        "id": 4,
+        "type": "text",
+        "content": "is just what I need 🌚!",
+        "x": "36",
+        "y": "83",
+        "s": "1.25",
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "color": "#ffffff",
+        "font": "Impact",
+        "emo": {
+          "enabled": false
+        }
+      }
+    ]
+  }
+,
+  "woods": {
+    "bg": {
+      "url": "",
+      "local": "temperatesage-forest-4395986.jpg",
+      "cover": true,
+      "x": 50,
+      "y": 50,
+      "s": 100
+    },
+    "elements": [
+      {
+        "id": 1,
+        "type": "emoji",
+        "content": "[RESPONDER]",
+        "x": 51,
+        "y": "47",
+        "s": 1.55,
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "emo": {
+          "enabled": true
+        }
+      },
+      {
+        "id": 2,
+        "type": "text",
+        "content": "Well, I think ",
+        "x": "26",
+        "y": "22",
+        "s": "1.05",
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "color": "#ffffff",
+        "font": "Impact",
+        "emo": {
+          "enabled": false
+        }
+      },
+      {
+        "id": 3,
+        "type": "text",
+        "content": "[GUESS_WORD]",
+        "x": 71,
+        "y": 22,
+        "s": "0.75",
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "color": "#ffffff",
+        "font": "Impact",
+        "emo": {
+          "enabled": false
+        }
+      },
+      {
+        "id": 4,
+        "type": "text",
+        "content": "is just what I need 🌚!",
+        "x": "29",
+        "y": "81",
+        "s": "1.15",
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "color": "#ffffff",
+        "font": "Impact",
+        "emo": {
+          "enabled": false
+        }
+      }
+    ]
+  }
+,
+  "report": {
+    "bg": {
+      "url": "https://cdn-useast1.kapwing.com/static/templates/charlie-conspiracy-always-sunny-in-philidelphia-meme-template-full-1b6bf049.webp",
+      "local": "",
+      "x": 32,
+      "y": "7",
+      "s": 107
+    },
+    "elements": [
+      {
+        "id": 1,
+        "type": "emoji",
+        "content": "[RESPONDER]",
+        "x": 34,
+        "y": "28",
+        "s": 1.85,
+        "r": "5",
+        "flipX": false,
+        "anim": "none",
+        "emo": {
+          "enabled": true
+        }
+      },
+      {
+        "id": 3,
+        "type": "text",
+        "content": "[GUESS_WORD]",
+        "x": "65",
+        "y": "16",
+        "s": "0.85",
+        "r": "7",
+        "flipX": false,
+        "anim": "none",
+        "color": "#ff0000",
+        "font": "Impact",
+        "emo": {
+          "enabled": false
+        }
+      }
+    ]
+  }
+,
+  "city": {
+    "bg": {
+      "url": "",
+      "local": "grzyzz-zhongjiang-bridge-991057.jpg",
+      "cover": true,
+      "x": 50,
+      "y": 50,
+      "s": 100
+    },
+    "elements": [
+      {
+        "id": 1,
+        "type": "emoji",
+        "content": "[RESPONDER]",
+        "x": 51,
+        "y": "38",
+        "s": 1.55,
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "emo": {
+          "enabled": true
+        }
+      },
+      {
+        "id": 2,
+        "type": "text",
+        "content": "Well, I think ",
+        "x": "24",
+        "y": "36",
+        "s": "0.8",
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "color": "#ffffff",
+        "font": "Impact",
+        "emo": {
+          "enabled": false
+        }
+      },
+      {
+        "id": 3,
+        "type": "text",
+        "content": "[GUESS_WORD]",
+        "x": "21",
+        "y": "54",
+        "s": "0.85",
+        "r": 0,
+        "flipX": false,
+        "anim": "none",
+        "color": "#ffffff",
+        "font": "Impact",
+        "emo": {
+          "enabled": false
+        }
+      },
+      {
+        "id": 4,
+        "type": "text",
+        "content": "is just what I need 🌚!",
+        "x": "38",
+        "y": "78",
+        "s": "1.3",
+        "r": "0",
+        "flipX": false,
+        "anim": "none",
+        "color": "#ffffff",
+        "font": "Impact",
+        "emo": {
+          "enabled": false
+        }
+      }
+    ]
+  }
+,
+  "spiritual": {
+    "bg": {
+      "url": "",
+      "local": "nikkigocom-hands-up-6321013.svg",
+      "cover": false,
+      "x": 50,
+      "y": "38",
+      "s": 100
+    },
+    "elements": [
+      {
+        "id": 3,
+        "type": "text",
+        "content": "꧁⎝ 𓆩༺✧༻𓆪 ⎠꧂",
+        "x": 50,
+        "y": "20",
+        "s": "0.9",
+        "r": "0",
+        "flipX": false,
+        "anim": "none",
+        "color": "#ffffff",
+        "font": "inherit",
+        "glow": true,
+        "emo": {
+          "enabled": false
+        },
+        "fixedText": true
+      },
+      {
+        "id": 2,
+        "type": "emoji",
+        "content": "[RESPONDER]",
+        "x": 50,
+        "y": 50,
+        "s": "2.05",
+        "r": 0,
+        "flipX": false,
+        "anim": "anim-float",
+        "color": "#ffffff",
+        "font": "inherit",
+        "emo": {
+          "enabled": true
+        }
+      }
+    ]
+  }
+,
+};
